26c26ab10b45c4757abc735ea045d5e483ef128f
Mark Harris <markymark1990@hotmail.co.ukgit config --global user.name markymark1990lsgit config --global user.email markymark1990@hotmail.co.uk>
2024-05-29 10:42:12 +0100

    completes task 3, GET /api

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,4 +1,9 @@
 const {fetchTopics} = require("../models/topics.model.js");
+const endpoints = require("../endpoints.json");
+
+exports.getEndpoints = (req, res) => {
+    res.status(200).send(endpoints)
+}
 
 exports.getTopics = (req, res, next) => {
     fetchTopics().then((topics) => {
