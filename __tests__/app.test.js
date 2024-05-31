fb16f8c891756558ad849b453ce9445d393e31f3
Mark Harris <markymark1990@hotmail.co.ukgit config --global user.name markymark1990lsgit config --global user.email markymark1990@hotmail.co.uk>
2024-05-31 09:55:40 +0100

    completes task 12

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -61,31 +61,11 @@ describe("GET /api/topics", () => {
 //Articles
 
 describe("GET /api/articles", () => {
-    it("200: responds with all articles except for 'body' sorted by date in descending order by default", () => {
+    it("200: responds with all articles except for 'body', sorted by date in descending order by default, including addition of comment count", () => {
         return request(app)
             .get("/api/articles")
             .expect(200)
-            .then((res) => {
-                expect(Array.isArray(res.body.articles)).toBe(true);
-                expect(res.body.articles.length).toBe(13);
-                res.body.articles.forEach(article => {
-                    expect(article).toHaveProperty('author');
-                    expect(article).toHaveProperty('title');
-                    expect(article).toHaveProperty('article_id');
-                    expect(article).not.toHaveProperty('body');
-                    expect(article).toHaveProperty('topic');
-                    expect(article).toHaveProperty('created_at');
-                    expect(article).toHaveProperty('votes');
-                    expect(article).toHaveProperty('article_img_url');
-                })
-                expect(res.body.articles).toBeSorted({ key: 'created_at', descending: true });
-            })
-    });
-    it("200: responds with all articles sorted by date in descending order omitting the body property and including a comment count", () => {
-        return request(app)
-            .get("/api/articles?properties=author,title,article_id,topic,created_at,votes,article_img_url")
-            .expect(200)
-            .then((res) => {
+            .then((res) => { 
                 expect(Array.isArray(res.body.articles)).toBe(true);
                 expect(res.body.articles.length).toBe(13);
                 res.body.articles.forEach(article => {
@@ -125,7 +105,7 @@ describe("GET /api/articles", () => {
 });
 
 describe("GET /api/articles/:article_id", () => {
-    it("200: responds with an article object from its id", () => {
+    it("200: responds with an article object from its id and includes comment_count", () => {
         return request(app)
             .get("/api/articles/1")
             .expect(200)
@@ -138,6 +118,7 @@ describe("GET /api/articles/:article_id", () => {
                 expect(res.body.article).toHaveProperty("created_at")
                 expect(res.body.article).toHaveProperty("votes")
                 expect(res.body.article).toHaveProperty("article_img_url")
+                expect(res.body.article).toHaveProperty("comment_count")
             });
     });
     it("404: Not Found when article doesnt exist", () => {
